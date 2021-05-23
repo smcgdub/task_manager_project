43ec814c948c099c952d8a13a553f1a48d2fb98a
smcgdub <stephen_xyz1@hotmail.com>
2021-05-23 16:23:57 +0000

    Updated html that wasnt working

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
 from flask import (
-      Flask, flash, render_template, 
-      redirect, request, session, url_for)
+    Flask, flash, render_template,
+    redirect, request, session, url_for)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 if os.path.exists("env.py"):
@@ -14,7 +14,6 @@ app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 app.secret_key = os.environ.get("SECRET_KEY")
 
-
 mongo = PyMongo(app)
 
 
@@ -22,7 +21,7 @@ mongo = PyMongo(app)
 @app.route("/get_tasks")
 def get_tasks():
     tasks = mongo.db.tasks.find()
-    return render_template("tasks.html", tasks=tasks) 
+    return render_template("tasks.html", tasks=tasks)
 
 
 if __name__ == "__main__":
