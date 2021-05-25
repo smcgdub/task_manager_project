3ca80bcd276d8e580fd6b4df196043d2e6fc2eca
smcgdub <stephen_xyz1@hotmail.com>
2021-05-25 16:05:05 +0000

    add task form input fields

--- static/js/script.js
+++ static/js/script.js
@@ -1,10 +1,22 @@
-  $(document).ready(function () {
-    $(".sidenav").sidenav({
-      edge: "right"
-    });
-    $('.collapsible').collapsible();
+// SIDE NAV OPEN
+$(document).ready(function () {
+  $(".sidenav").sidenav({
+    edge: "right"
   });
-
-  $(document).ready(function () {
-    $('.tooltipped').tooltip();
+  $('.collapsible').collapsible();
+});
+// URGENT ICON
+$(document).ready(function () {
+  $('.tooltipped').tooltip();
+});
+// DATE PICKER
+$(document).ready(function () {
+  $('.datepicker').datepicker({
+    format: "dd mmmm, yyyy",
+    yearRange: 3,
+    showClearBtn: true,
+    i18n: {
+      done: "Select"
+    }
   });
+});
